07968f7883085bfbafa8086f9bc06e388ace372f
Nicola Jordan <njordan.hsr@gmail.com>
2017-02-03 14:57:22 +0100

    use percentages for range

--- test_example_conversions.py
+++ test_example_conversions.py
@@ -11,8 +11,9 @@ def _test_pandoc(result_file, command, *, expected_file_size, size_deviation=0):
         assert os.path.exists(result_file)
         assert expected_file_size > 200  # should always contain data!
         if size_deviation > 0:
-            min_size = expected_file_size - size_deviation
-            max_size = expected_file_size + size_deviation
+            difference = size_deviation * expected_file_size
+            min_size = expected_file_size - difference
+            max_size = expected_file_size + difference
             assert min_size < actual_file_size < max_size
         else:
             assert expected_file_size == actual_file_size
@@ -24,14 +25,14 @@ def _test_pandoc(result_file, command, *, expected_file_size, size_deviation=0):
 
 def test_html_generation():
     to_html = 'docker run --rm -v "$(pwd):/pandoc" geometalab/pandoc pandoc -o {} README.md'
-    _test_pandoc('README.html', to_html, expected_file_size=1457, size_deviation=200)
+    _test_pandoc('README.html', to_html, expected_file_size=1457, size_deviation=0.1)
 
 
 def test_pdf_generation():
     to_pdf = 'docker run --rm -v "$(pwd):/pandoc" geometalab/pandoc pandoc -o {} README.md'
-    _test_pandoc('README.pdf', to_pdf, expected_file_size=106515, size_deviation=200)
+    _test_pandoc('README.pdf', to_pdf, expected_file_size=106515, size_deviation=0.1)
 
 
 def test_pdf_generation_using_html5_and_wkhtml():
     to_pdf = 'docker run --rm -v "$(pwd):/pandoc" geometalab/pandoc bash -c "pandoc -t html5 README.md | wkhtmltopdf - {}"'
-    _test_pandoc('README.pdf', to_pdf, expected_file_size=22349, size_deviation=200)
+    _test_pandoc('README.pdf', to_pdf, expected_file_size=20980, size_deviation=0.1)
